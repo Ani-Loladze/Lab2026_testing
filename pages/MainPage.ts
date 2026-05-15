7d52414ef9249638f37e26ba169dd09b9b713361
Ani-Loladze <ani_loladze@epam.com>
2026-05-15 17:14:39 +0400

    Created first test - verifying one dropdown

--- pages/MainPage.ts
+++ pages/MainPage.ts
@@ -1,18 +1,39 @@
-import { Page } from '@playwright/test';
-import { BASE_URL } from '../utils/testConfig';
+import { Page, Locator } from '@playwright/test';
 
 export class MainPage {
   private page: Page;
+  readonly exploreDiscography: Locator;
+  private exploreDropdown: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.exploreDiscography = page
+      .getByRole('navigation')
+      .getByRole('button', { name: 'Explore Discography' });
+    this.exploreDropdown = page.getByRole('menu');
   }
 
   async open(): Promise<void> {
-    await this.page.goto(BASE_URL);
+    await this.page.goto('/');
   }
 
   async getTitle(): Promise<string> {
-    return await this.page.title();
+    return this.page.title();
   }
+
+  async openExploreDiscography(): Promise<void> {
+    await this.exploreDiscography.click();
+    await this.exploreDropdown.waitFor({ state: 'visible' });
+  }
+
+  getExploreDiscographyOptions() {
+  return {
+    exploreAll: this.page.getByRole('menuitem', { name: 'Explore All' }),
+    advancedSearch: this.page.getByRole('menuitem', { name: 'Advanced Search' }),
+    mostCollected: this.page.getByRole('menuitem', { name: 'Most Collected' }),
+    submitRelease: this.page.getByRole('menuitem', { name: 'Submit a Release' }),
+    submissionGuidelines: this.page.getByRole('menuitem', { name: 'Submission Guidelines' }),
+  };
 }
+}
+
